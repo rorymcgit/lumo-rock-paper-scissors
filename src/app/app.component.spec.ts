585e20c3cf0e74952069d6d0062b4fc50724afa7
rorymcgit <rorymcguinness@hotmail.co.uk>
2018-08-01 16:33:18 +0100

    refactor to Move type, set user + computer choices

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -8,24 +9,28 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('creates the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'Rock-Paper-Scissors'`, async(() => {
+
+  it(`has title 'Rock-Paper-Scissors'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Rock-Paper-Scissors');
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('renders title in h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Let\'s play Rock-Paper-Scissors!');
   }));
-  describe('throw buttons', () => {
-    it('should have all the images', async(() => {
+
+  describe('choice buttons', () => {
+    it('image src paths should be correct', async(() => {
       const fixture = TestBed.createComponent(AppComponent);
       fixture.detectChanges();
 
@@ -35,9 +40,32 @@ describe('AppComponent', () => {
       const imageSourceTwo = images[1].getAttribute('src');
       const imageSourceThree = images[2].getAttribute('src');
 
-      expect(imageSourceOne).toEqual('/src/assets/rock.png');
-      expect(imageSourceTwo).toEqual('/src/assets/paper.png');
-      expect(imageSourceThree).toEqual('/src/assets/scissors.png');
+      expect(imageSourceOne).toEqual('/src/assets/rock.jpg');
+      expect(imageSourceTwo).toEqual('/src/assets/paper.jpg');
+      expect(imageSourceThree).toEqual('/src/assets/scissors.jpg');
+    }));
+
+    it('being clicked sets user selection', async( () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const component = fixture.componentInstance;
+
+      const compiled = fixture.debugElement.nativeElement;
+      const rockBtn = compiled.querySelector('button'); // get first button
+      rockBtn.click();
+
+      expect(component.userChoice).toEqual('rock');
+    }));
+  });
+
+  describe('setComputerChoice()', () => {
+    it('always returns either "rock", "paper" or "scissors"', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+
+      const component = fixture.componentInstance;
+
+      expect(component.setComputerChoice()).toMatch(/rock|paper|scissors/);
     }));
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+type Move = 'rock' | 'paper' | 'scissors';
+
+interface WinningMoves {
+  rock: Move;
+  paper: Move;
+  scissors: Move;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +15,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Rock-Paper-Scissors';
+  userChoice: Move;
+  choices: Move[];
+  winningMoves: WinningMoves;
+
+  constructor() {
+    this.choices = ['rock', 'paper', 'scissors'];
+
+    this.winningMoves = {
+      rock: 'scissors',
+      paper: 'rock',
+      scissors: 'paper'
+    };
+  }
+
+  setUserChoice(m: Move) {
+    // DEBUG
+    console.log('clicked: ', m);
+
+    this.userChoice = m;
+  }
+
+  setComputerChoice(): Move {
+    const randomIndex = Math.floor(Math.random() * 3);
+
+    // DEBUG
+    console.log('computer chose: ', this.choices[randomIndex]);
+
+    return this.choices[randomIndex];
+  }
 }
